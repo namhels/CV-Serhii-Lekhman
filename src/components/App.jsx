242e1c64a05f7e137112265c693995ffddfb27d7
namhels <8.lsv.888@gmail.com>
2023-07-01 17:05:24 +0300

    start CV

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import { Avatar, Flex, Grid, GridItem } from '@chakra-ui/react';
+
+import SerhiiLekhman from '../images/SerhiiLekhman.jpg';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Flex justify="center">
+      <Grid
+        templateAreas={`"aside header"
+                      "aside main"`}
+        templateRows={'370px 1fr'}
+        templateColumns={'370px 1fr'}
+        w="1200px"
+        bg="tomato"
+      >
+        <GridItem bg="pink.300" area={'aside'}>
+          <Flex justify="center" pt="20px">
+            <Avatar
+              w="300px"
+              h="300px"
+              name="Serhii Lekhman"
+              src={SerhiiLekhman}
+            />
+          </Flex>
+        </GridItem>
+        <GridItem mt={8} bg="green.300" area={'header'}>
+          Header
+        </GridItem>
+        <GridItem bg="blue.300" area={'main'}>
+          Main
+        </GridItem>
+      </Grid>
+    </Flex>
   );
 };
