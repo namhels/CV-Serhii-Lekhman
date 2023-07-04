e478c0b8785c4b7e14054c4860665af5d7a319c1
namhels <8.lsv.888@gmail.com>
2023-07-04 18:42:01 +0300

    add contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,18 @@
-import { Avatar, Flex, Grid, GridItem } from '@chakra-ui/react';
+import {
+  Avatar,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  List,
+  ListIcon,
+  ListItem,
+  VStack,
+} from '@chakra-ui/react';
+
+import { GiSmartphone } from 'react-icons/gi';
+import { MdOutlineEmail, MdLocationPin } from 'react-icons/md';
+import { AiFillGithub, AiOutlineLinkedin } from 'react-icons/ai';
 
 import SerhiiLekhman from '../images/SerhiiLekhman.jpg';
 
@@ -14,7 +28,7 @@ export const App = () => {
         bg="tomato"
       >
         <GridItem bg="pink.300" area={'aside'}>
-          <Flex justify="center" pt="20px">
+          <Flex justify="center" pt="40px">
             <Avatar
               w="300px"
               h="300px"
@@ -22,6 +36,35 @@ export const App = () => {
               src={SerhiiLekhman}
             />
           </Flex>
+          <VStack spacing={4} py={16}>
+            <Flex direction="column" align="start" spacing={4}>
+              <Heading as="h2" size="lg">
+                Contacts
+              </Heading>
+              <List spacing={3}>
+                <ListItem>
+                  <ListIcon as={GiSmartphone} />
+                  +38 095-555-90-63
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={MdOutlineEmail} />
+                  8.lsv.888@gmail.com
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={AiFillGithub} />
+                  Quidem, ipsam
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={AiOutlineLinkedin} />
+                  Quidem, ipsam
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={MdLocationPin} color="green.500" />
+                  Ukraine, Zaporizhzhya
+                </ListItem>
+              </List>
+            </Flex>
+          </VStack>
         </GridItem>
         <GridItem mt={8} bg="green.300" area={'header'}>
           Header
