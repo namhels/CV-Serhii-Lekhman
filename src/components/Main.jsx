b1e1fcf22bbb13dacedef71863b044e456a4a8b8
Serhii Lekhman <namhels@MacBook-Pro-Serhii.local>
2023-11-12 12:27:27 +0100

    update PROJECT EXPERIENCE

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,9 +10,11 @@ import {
   ListIcon,
   Text,
   Flex,
+  IconButton,
 } from '@chakra-ui/react';
 
 import { AiFillGithub } from 'react-icons/ai';
+import { FaRegEye } from 'react-icons/fa';
 import projects from '../db/projects.json';
 import works from '../db/works.json';
 import educations from '../db/educations.json';
@@ -42,23 +44,40 @@ const Main = () => {
         </AbsoluteCenter>
       </Box>
       <UnorderedList spacing={4} mt={3}>
-        {projects.map(({ href, heading, technologies, title, description }) => (
-          <ListItem>
-            <Flex justifyContent="space-between">
-              <Link href={href} isExternal>
-                <Flex alignItems="baseline">
-                  <Heading as="h3" size="md">
-                    {heading}
-                  </Heading>
-                  <ListIcon as={AiFillGithub} ml="10px" />
+        {projects.map(
+          ({ href, hrefView, heading, technologies, title, description }) => (
+            <ListItem key={heading}>
+              <Flex justifyContent="space-between">
+                <Flex alignItems="center">
+                  <Link href={href} isExternal>
+                    <Flex alignItems="center">
+                      <ListIcon as={AiFillGithub} mr="5px" />
+                      <Heading as="h3" size="md">
+                        {heading}
+                      </Heading>
+                    </Flex>
+                  </Link>
+                  <Link href={hrefView} isExternal>
+                    <IconButton
+                      mx="8px"
+                      isRound={true}
+                      variant="outline"
+                      colorScheme="black"
+                      aria-label="View project"
+                      boxSize="16px"
+                      height="20px"
+                      fontSize="16px"
+                      icon={<FaRegEye />}
+                    />
+                  </Link>
                 </Flex>
-              </Link>
-              <Text fontSize="md">{technologies}</Text>
-            </Flex>
-            <Text fontSize="md">{title}</Text>
-            <Text fontSize="md">{description}</Text>
-          </ListItem>
-        ))}
+                <Text fontSize="md">{technologies}</Text>
+              </Flex>
+              <Text fontSize="md">{title}</Text>
+              <Text fontSize="md">{description}</Text>
+            </ListItem>
+          )
+        )}
       </UnorderedList>
       <Box position="relative" padding="5" mt={10}>
         <Divider borderBottomWidth="3px" borderColor="black" />
@@ -76,7 +95,7 @@ const Main = () => {
       </Box>
       <UnorderedList spacing={4} mt={3}>
         {works.map(({ position, firm, years, description }) => (
-          <ListItem>
+          <ListItem key={position}>
             <Heading as="h3" size="md">
               {position}
             </Heading>
@@ -106,7 +125,7 @@ const Main = () => {
       </Box>
       <UnorderedList spacing={4} mt={3}>
         {educations.map(({ title, years, description }) => (
-          <ListItem>
+          <ListItem key={title}>
             <Flex justifyContent="space-between">
               <Heading as="h4" size="md">
                 {title}
